23e050101b2b86f6958b7971157161e8caf3a64c
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-14 19:58:29 -0300

    update(main): added a condition to update and init the black window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include "cub3d.h"
 
-int	main(void)
+int	main(int c, char **v)
 {
-	printf("hello, world!");
+	t_cub	cub;
+
+	if (c == 2 && !ft_strncmp(v[1], "test", 4))
+	{
+		cub_init(&cub);
+		mlx_put_image_to_window(cub.mlx_connection, cub.mlx_window, cub.img.img_ptr, 0, 0);
+		mlx_loop(cub.mlx_connection);
+	}
+	else
+	{
+		printf("pode nn man! :(\n");
+		exit(EXIT_FAILURE);
+	}
 	return (0);
 }
