89ce85cdd09c0c376e5e7b5bf84ce643d29812f7
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-14 13:26:50 -0300

    update(prototype n libraries): added a new prototype and new libraries to the headers

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,8 +1,11 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include <unistd.h>
 # include <stdio.h>
 # include <stdbool.h>
+# include <stdlib.h>
+# include <stddef.h>
 # include <math.h>
 # include <X11/X.h>
 # include <X11/keysym.h>
--- includes/libft.h
+++ includes/libft.h
@@ -5,6 +5,7 @@
 # include "cub3d.h"
 
 // Libft/libft.c
-bool			ft_isspace(int c);
+bool	ft_isspace(int c);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
