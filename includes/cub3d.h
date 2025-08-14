ab10772ae5e11e9afd7fa454e06b529bc70bec73
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-14 12:22:15 -0300

    feat(includes): created a new folder to add the the header files, this one is the core header

---
+++ includes/cub3d.h
@@ -0,0 +1,13 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <stdbool.h>
+# include <math.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include "../minilibx-linux/mlx.h"
+
+# include "libft.h"
+
+#endif
