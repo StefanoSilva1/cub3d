1785ddaf8294c19fb98e38f8c7675f50d012c7ae
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-14 19:26:04 -0300

    feat(ft_strncmp): added to get the string and compare with the input int the terminal

---
+++ Libft/libft/ft_strncmp.c
@@ -0,0 +1,17 @@
+#include "cub3d.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	index;
+
+	index = 0;
+	while (s1[index] != '\0' && s2[index] != '\0' && index < n)
+	{
+		if (s1[index] != s2[index])
+			return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+		index++;
+	}
+	if (index < n)
+		return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+	return (0);
+}
