35cd24c2030a9f9ea5597b3021d0d12eff4a643d
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-20 00:14:53 -0300

    udpate(comments): added comments to understand some MLX functions

--- src/init_files/init.c
+++ src/init_files/init.c
@@ -31,20 +31,20 @@ static void	events_init(t_cub *cub)
 
 void	cub_init(t_cub *cub)
 {
-	cub->mlx_connection = mlx_init();
+	cub->mlx_connection = mlx_init(); //inicializa a a conexao com a bilioteca
 	if (!cub->mlx_connection)
 		err_exit(cub);
 	cub->mlx_window = mlx_new_window(cub->mlx_connection, \
-		WIDTH, HEIGHT, "cub3D");
+		WIDTH, HEIGHT, "cub3D");  //cria a janela com alt. larg. definidos
 	if (!cub->mlx_window)
 		err_exit(cub);
-	cub->img.img_ptr = mlx_new_image(cub->mlx_connection, WIDTH, HEIGHT);
+	cub->img.img_ptr = mlx_new_image(cub->mlx_connection, WIDTH, HEIGHT); //inicializa a imagem, usamos data_addr para desenhar 
 	if (!cub->img.img_ptr)
 		err_exit(cub);
 	cub->img.pxl_ptr = mlx_get_data_addr(cub->img.img_ptr, \
 	&cub->img.bits_per_pxl, \
 	&cub->img.l_len, \
-	&cub->img.endian);
+	&cub->img.endian); //obtem acesso aos pixels da imagem para altera-los de maneira mais eficiente
 	if (!cub->img.pxl_ptr)
 		err_exit(cub);
 	events_init(cub);
