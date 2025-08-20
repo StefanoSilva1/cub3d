7b67d3faa4d29b079550c5347d4fd7ef6239765d
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-20 16:54:34 -0300

    feat(init_player): added new function to get the point-of-view of the player, and where it need to look at the start of the map, set NO_SO_ES_WE, at the set_player_direction(), to init the members of the struct s_player

---
+++ src/init_files/init_player.c
@@ -0,0 +1,69 @@
+#include "cub3d.h"
+
+static void	set_NO_or_SO_dir(t_cub *cub, char direction)
+{
+	if (direction == 'N')
+	{
+		cub->player.dir_y = -1;
+		cub->player.dir_x = 0;
+		cub->player.plane_y = 0;
+		cub->player.plane_x = 0.66;
+	}
+	else if (direction == 'S')
+	{
+		cub->player.dir_y = 1;
+		cub->player.dir_x = 0;
+		cub->player.plane_y = 0;
+		cub->player.plane_x = -0.66;
+	}
+}
+
+static void	set_ES_or_WE_dir(t_cub *cub, char direction)
+{
+	if (direction == 'E')
+	{
+		cub->player.dir_y = 0;
+		cub->player.dir_x = 1;
+		cub->player.plane_y = 0.66;
+		cub->player.plane_x = 0;
+	}
+	else if (direction == 'W')
+	{
+		cub->player.dir_y = 0;
+		cub->player.dir_x = -1;
+		cub->player.plane_y = -0.66;
+		cub->player.plane_x = 0;	
+	}
+}
+
+static void	set_player_direction(t_cub *cub, char direction)
+{
+	if (direction == 'N' || direction == 'S')
+		set_NO_or_SO_dir(cub, direction);
+	else if (direction == 'E' || direction == 'W')
+		set_ES_or_WE_dir(cub, direction);
+}
+
+void	init_player(t_cub *cub)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	while (cub->map[y])
+	{
+		x = 0;
+		while (cub->map[y][x])
+		{
+			if (ft_strchr("NSEW", cub->map[y][x]))
+			{
+				cub->player.pos_x = x + 0.5;
+				cub->player.pos_y = y + 0.5;
+				set_player_direction(cub, cub->map[y][x]);
+				return ;
+			}
+			x++;
+		}
+		y++;
+	}
+}
