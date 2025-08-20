389c580c2e49b17af1601fdc532264f8d9e29770
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-20 01:54:45 -0300

    feat(render): added ceiling n' floor at the project

---
+++ src/render/render.c
@@ -0,0 +1,29 @@
+#include "cub3d.h"
+
+void	draw_background(t_cub *cub)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			if (y < HEIGHT / 2)
+				put_pxl_in_img(&cub->img, x, y, cub->sprite.ceilling_color);
+			else 
+				put_pxl_in_img(&cub->img, x, y, cub->sprite.floor_color);
+			x++;
+		}
+		y++;
+	}
+}
+
+int	render(t_cub *cub)
+{
+	draw_background(cub);
+	mlx_put_image_to_window(cub->mlx_connection, cub->mlx_window, cub->img.img_ptr, 0, 0); //exibe a imagem na tela
+	return (0);
+}
