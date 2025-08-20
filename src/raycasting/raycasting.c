308182b36c8d5b3efa2051be671d4cc5595b487d
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-20 19:42:18 -0300

    feat(DDA): started to implementen the DDA logic

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -1,5 +1,64 @@
 #include "cub3d.h"
 
+static void	get_left_or_right_side(t_cub *cub, t_ray *ray, double ray_dir_y, double ray_dir_x)
+{
+	if (ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dst_x = (cub->player.pos_x - ray->map_x) * ray->delta_dst_x;
+	}
+	else
+	{
+	    ray->step_x = 1;
+	    ray->side_dst_x = (ray->map_x + 1.0 - cub->player.pos_x) * ray->delta_dst_x;
+	}
+	if (ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dst_y = (cub->player.pos_y - ray->map_y) * ray->delta_dst_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dst_y = (ray->map_y + 1.0 - cub->player.pos_y) * ray->delta_dst_y;
+	}
+}
+
+static void	init_ray(t_cub *cub, t_ray *ray, double ray_dir_y, double ray_dir_x)
+{
+	ray->map_y = (int)cub->player.pos_y;
+	ray->map_x = (int)cub->player.pos_x;
+	ray->delta_dst_y = fabs(1 / ray_dir_y);
+	ray->delta_dst_x = fabs(1 / ray_dir_x);
+}
+
+t_ray	digital_differential_analizer(t_cub *cub, double ray_dir_y, double ray_dir_x)
+{
+	t_ray	ray;
+
+	init_ray(cub, &ray, ray_dir_y, ray_dir_x);
+	get_left_or_right_side(cub, &ray, ray_dir_y, ray_dir_x);
+	ray.hit = 0;
+	while (ray.hit == 0)
+	{
+		if (ray.side_dst_x < ray.side_dst_y)
+		{
+			ray.side_dst_x += ray.delta_dst_x;
+			ray.map_x += ray.step_x;
+			ray.side = 0;
+		}
+		else
+		{
+			ray.side_dst_y += ray.delta_dst_y;
+			ray.map_y += ray.step_y;
+			ray.side = 1;
+		}
+		if (cub->map[ray.map_y][ray.map_x] == '1')
+			ray.hit = 1;
+	}
+	return (ray);
+}
+
 void	raycasting(t_cub *cub, int x)
 {
 	double	camera_x;
