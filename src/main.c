399bf9f673c5bcaf89d2153deed4ade463ee9fa7
Stefano Davi Almeida Silva <sdavi-al@c2r7p5.42sp.org.br>
2025-08-20 14:23:42 -0300

    up

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sdavi-al <sdavi-al@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 10:59:25 by sdavi-al          #+#    #+#             */
-/*   Updated: 2025/08/20 14:04:39 by sdavi-al         ###   ########.fr       */
+/*   Updated: 2025/08/20 14:08:23 by sdavi-al         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int argc, char **argv)
 
 	ft_bzero(&cub, sizeof(cub));
 	if (argc != 2 || !valid_format(argv[1]))
-		error_handler(&cub, "Error: Invalid arguments\nUsage: ./cub3D map.cub\n");
+		error_handler(&cub, "Error: Invalid arguments\nUsage: ./cub3D maps/map.cub\n");
 	cub_init(&cub);
 	mlx_put_image_to_window(cub.mlx_connection, cub.mlx_window, cub.img.img_ptr, 0, 0);
 	mlx_loop(cub.mlx_connection);
