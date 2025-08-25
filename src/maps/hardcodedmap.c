6627701837d4a89199e9c09c85c64c6037e8b38c
Guildguy <fabiolljunior23@gmail.com>
2025-08-25 18:28:31 -0300

    update(map): change FOV from the player in the hardmap

--- src/maps/hardcodedmap.c
+++ src/maps/hardcodedmap.c
@@ -20,10 +20,10 @@ char	**create_hard_map(void)
 
 	map = ft_calloc(8, sizeof(char *));
 	map[0] = ft_strdup("111111111");
-	map[1] = ft_strdup("100000001");
+	map[1] = ft_strdup("1E0000001");
 	map[2] = ft_strdup("101000101");
 	map[3] = ft_strdup("111000111");
-	map[4] = ft_strdup("100N00001");
+	map[4] = ft_strdup("100000001");
 	map[5] = ft_strdup("101000101");
 	map[6] = ft_strdup("111111111");
 	map[7] = NULL;
