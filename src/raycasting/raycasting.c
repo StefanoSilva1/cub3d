5e40c9b8264a78cdc55c851c83f076f8ef9d06db
Guildguy <fabiolljunior23@gmail.com>
2025-08-25 17:50:04 -0300

    update(raycasting): raycasting DDA call to get the ray distance at the walls

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -70,8 +70,10 @@ void	raycasting(t_cub *cub, int x)
 	double	camera_x;
 	double	ray_dir_y;
 	double	ray_dir_x;
+	t_ray	ray_result;
 
 	camera_x = 2 * x / (double)WIDTH - 1;
 	ray_dir_x = cub->player.dir_x + cub->player.plane_x * camera_x;
 	ray_dir_y = cub->player.dir_y + cub->player.plane_y * camera_x;
+	ray_result = digital_differential_analizer(cub, ray_dir_y, ray_dir_x);
 }
