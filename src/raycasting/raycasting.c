7023f25b0b306c142d37c4f01196275ce51873a0
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-26 11:44:45 -0300

    update(struct name): changed var name from: plane, to: cam_plane

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -1,5 +1,19 @@
 #include "cub3d.h"
 
+static void	init_ray(t_cub *cub, t_ray *ray, double ray_dir_y, double ray_dir_x)
+{
+	ray->map_y = (int)cub->player.pos_y;
+	ray->map_x = (int)cub->player.pos_x;
+	if (ray_dir_x == 0)
+		ray->delta_dst_x = 1e30;
+	else
+		ray->delta_dst_x = fabs(1 / ray_dir_x);
+	if (ray_dir_y == 0)
+		ray->delta_dst_y = 1e30;
+	else
+		ray->delta_dst_y = fabs(1 / ray_dir_y);
+}
+
 static void	get_left_or_right_side(t_cub *cub, t_ray *ray, double ray_dir_y, double ray_dir_x)
 {
 	if (ray_dir_x < 0)
@@ -22,20 +36,6 @@ static void	get_left_or_right_side(t_cub *cub, t_ray *ray, double ray_dir_y, dou
 		ray->step_y = 1;
 		ray->side_dst_y = (ray->map_y + 1.0 - cub->player.pos_y) * ray->delta_dst_y;
 	}
-}
-
-static void	init_ray(t_cub *cub, t_ray *ray, double ray_dir_y, double ray_dir_x)
-{
-	ray->map_y = (int)cub->player.pos_y;
-	ray->map_x = (int)cub->player.pos_x;
-	if (ray_dir_x == 0)
-		ray->delta_dst_x = 1e30;
-	else
-		ray->delta_dst_x = fabs(1 / ray_dir_x);
-	if (ray_dir_y == 0)
-		ray->delta_dst_y = 1e30;
-	else
-		ray->delta_dst_y = fabs(1 / ray_dir_y);
 }
 
 t_ray	digital_differential_analizer(t_cub *cub, double ray_dir_y, double ray_dir_x)
@@ -73,8 +73,8 @@ void	raycasting(t_cub *cub, int x)
 	t_ray	ray_result;
 
 	camera_x = 2 * x / (double)WIDTH - 1;
-	ray_dir_x = cub->player.dir_x + cub->player.plane_x * camera_x;
-	ray_dir_y = cub->player.dir_y + cub->player.plane_y * camera_x;
+	ray_dir_x = cub->player.dir_x + cub->player.cam_plane_x * camera_x;
+	ray_dir_y = cub->player.dir_y + cub->player.cam_plane_y * camera_x;
 	ray_result = digital_differential_analizer(cub, ray_dir_y, ray_dir_x);
 	draw_wall(cub, x, &ray_result);
 }
