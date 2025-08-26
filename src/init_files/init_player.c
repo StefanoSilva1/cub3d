4db4dc080bc637489680f37f83f875901c98d3b2
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-26 11:44:16 -0300

    update(struct name): changed var name from: plane, to: cam_plane

--- src/init_files/init_player.c
+++ src/init_files/init_player.c
@@ -5,22 +5,22 @@ static void	set_player_direction(t_cub *cub, char direction)
 	if (direction == 'N')
 	{
 		cub->player.dir_y = -1;
-		cub->player.plane_x = 0.66;
+		cub->player.cam_plane_x = 0.666;
 	}
 	else if(direction == 'S')
 	{
 		cub->player.dir_y = 1;
-		cub->player.plane_x = -0.66;
+		cub->player.cam_plane_x = -0.666;
 	}
 	else if (direction == 'E')
 	{
 		cub->player.dir_x = 1;
-		cub->player.plane_y = 0.66;
+		cub->player.cam_plane_y = 0.666;
 	}
 	else if (direction == 'W')
 	{
 		cub->player.dir_x = -1;
-		cub->player.plane_y = -0.66;
+		cub->player.cam_plane_y = -0.666;
 	}
 }
 
