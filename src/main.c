b62ae6172430435de631ebe32f4ecd4a77102512
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-26 11:55:47 -0300

    update(main): modularized n' arg value fixed

--- src/main.c
+++ src/main.c
@@ -4,23 +4,14 @@ int	main(int c, char **v)
 {
 	t_cub	cub;
 
-	if (c == 2 && !ft_strncmp(v[1], "init", 4))
-	{
-		ft_bzero(&cub, sizeof(t_cub));
-		cub.map = create_hard_map();
-		//adicionar em outra funcao posteriormente, isso mexe com inicializacao
-		cub.sprite.ceilling_color = create_rgb(0, 135, 206, 235);
-		cub.sprite.floor_color = create_rgb(0, 128, 200, 128);
-		//---------------------------------------------------//
-		init_player(&cub);
-		cub_init(&cub);
-		mlx_loop_hook(cub.mlx_connection, render, &cub); //chama a funcao e fica re-desenhando as iteracoes
-		mlx_loop(cub.mlx_connection); //inicia a renderizacao
-	}
-	else
+	if (c != 2 || ft_strncmp(v[1], "init", 4) != 0)
 	{
 		printf("pode nn man! :(\n");
 		exit(EXIT_FAILURE);
 	}
+	init_data(&cub);
+	cub_init(&cub);
+	mlx_loop_hook(cub.mlx_connection, render, &cub); //chama a funcao e fica re-desenhando as iteracoes
+	mlx_loop(cub.mlx_connection); //inicia a renderizacao
 	return (0);
 }
